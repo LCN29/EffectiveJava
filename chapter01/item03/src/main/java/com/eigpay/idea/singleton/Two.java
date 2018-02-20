1277b18fd7d61411930002c289b3c278a1874508
canxin.li <licanxin@eigpay.net>
2018-02-20 21:32:55 +0800

    加了一个注释

--- chapter01/item03/src/main/java/com/eigpay/idea/singleton/Two.java
+++ chapter01/item03/src/main/java/com/eigpay/idea/singleton/Two.java
@@ -9,7 +9,7 @@ package com.eigpay.idea.singleton;
  * 同时使用了静态工厂的方式:
  * （1）工厂方法返回唯一的实例，但是可以修改，比如，返回调用它的每个线程的单独实例等，而不用修改其他的Api，
  * （2）如果你的应用程序需要它，可以编写一个泛型单例工厂，
- * （3）方法引用可以用supplier，例如Elvis :: instance等同于Supplier<Elvis>
+ * （3）方法引用可以用supplier，例如Elvis :: instance等同于Supplier<Elvis>（方法引用）
  */
 public class Two {
 
